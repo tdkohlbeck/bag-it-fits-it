7f3f13a1c7b53df5fba5b2d804750aadaa5bb917
Travis Kohlbeck <tdkohlbeck+github@gmail.com>
2017-06-13 14:08:26 -0400

    generates csv!

--- bag-it-fits-it.py
+++ bag-it-fits-it.py
@@ -2,11 +2,11 @@
 
 import shutil, os, sys, subprocess
 import bagit
-import xmltodict, json
+import json, csv, xmltodict, jsonstocsv
 
 dirToBag = sys.argv[1]
-archiveBagDir = sys.argv[2] + '/archive-bag'
-examineBagDir = sys.argv[2] + '/examine-bag'
+archiveBagDir = sys.argv[2] + '/master-bag'
+examineBagDir = sys.argv[2] + '/working-bag'
 fitsXmlDir = sys.argv[2] + '/fits-xml/'
 
 shutil.copytree(dirToBag, archiveBagDir)
@@ -39,12 +39,47 @@ flatJson = flattenJson(json.loads(jsonStr), '__')
 #jsonFile.write(json.dumps(flatJson, indent=2))
 #jsonFile.close()
 
+fitsDicts = []
 for filename in os.listdir(fitsXmlDir):
-    print(fitsXmlDir + filename)
-    report = open(fitsXmlDir + filename)
-    obj = xmltodict.parse(report.read())
-    report.close()
-    reports = open(sys.argv[2] + 'reports.json', 'a+')
-    flatJson = flattenJson(obj, '__')
-    reports.write(json.dumps(flatJson, indent=2))
-    reports.close()
+    fitsXmlReport = open(fitsXmlDir + filename)
+    fitsDict = xmltodict.parse(fitsXmlReport.read())
+    fitsXmlReport.close()
+
+    flatFitsDict = flattenJson(fitsDict, '/')
+
+    fitsDicts.append(flatFitsDict)
+    allFitsReports = open(sys.argv[2] + 'reports.json', 'a+')
+    allFitsReports.write(json.dumps(flatJson, indent=2))
+    allFitsReports.write(',\n')
+    allFitsReports.close()
+
+#csvFitsReport = jsons-to-csv(flatJsonFitsReports)
+
+headers = []
+for fitsDict in fitsDicts:
+    for key in fitsDict.keys():
+        if key not in headers: headers.append(key)
+
+csvFile = open(sys.argv[2]+'report.csv', 'w')
+pen = csv.writer(csvFile)
+pen.writerow(headers)
+
+for fitsDict in fitsDicts:
+    print('fitsDicts len: ' + str(len(fitsDicts)))
+    row = []
+    currentColumn = 0
+    for key in fitsDict.keys():
+        print('fitsDict len: ' + str(len(fitsDict.keys())))
+        #print(json.dumps(fitsDict, indent=2))
+        if key == headers[currentColumn]:
+            print('currentColumn: ' + str(currentColumn))
+            print('row length: ' + str(len(row)))
+            row.append(fitsDict[key])
+        else:
+            row.append('')
+        currentColumn += 1
+    pen.writerow(row)
+
+csvFile.close()
+
+print(json.dumps(open(sys.argv[2]+'reports.json').read(), indent=2))
