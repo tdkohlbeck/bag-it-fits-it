9b8a35e40fa92d7ee1f23e5c3126dd07ea82c460
Travis Kohlbeck <tdkohlbeck+github@gmail.com>
2017-06-13 14:37:56 -0400

    dict to csv, skips json step

--- bag-it-fits-it.py
+++ bag-it-fits-it.py
@@ -22,41 +22,35 @@ xml = open(fitsXmlDir + 'bagit.txt.fits.xml')
 jsonStr = json.dumps(xmltodict.parse(xml.read()))
 xml.close()
 
-def flattenJson(obj, delim):
+def flattenDict(obj, delim):
     val = {}
     for i in obj.keys():
         if isinstance(obj[i], dict):
-            get = flattenJson(obj[i], delim)
+            get = flattenDict(obj[i], delim)
             for j in get.keys():
                 val[ i + delim + j ] = get[j]
         else:
             val[i] = obj[i]
     return val
 
-flatJson = flattenJson(json.loads(jsonStr), '__')
+flatJson = flattenDict(json.loads(jsonStr), '__')
 
 #jsonFile = open(fitsXmlDir + 'report.json', 'w+')
 #jsonFile.write(json.dumps(flatJson, indent=2))
 #jsonFile.close()
 
-fitsDicts = []
+flatFitsDicts = []
 for filename in os.listdir(fitsXmlDir):
     fitsXmlReport = open(fitsXmlDir + filename)
     fitsDict = xmltodict.parse(fitsXmlReport.read())
     fitsXmlReport.close()
+    flatFitsDict = flattenDict(fitsDict, ' / ')
+    flatFitsDicts.append(flatFitsDict)
 
-    flatFitsDict = flattenJson(fitsDict, '/')
 
-    fitsDicts.append(flatFitsDict)
-    allFitsReports = open(sys.argv[2] + 'reports.json', 'a+')
-    allFitsReports.write(json.dumps(flatJson, indent=2))
-    allFitsReports.write(',\n')
-    allFitsReports.close()
-
-#csvFitsReport = jsons-to-csv(flatJsonFitsReports)
 
 headers = []
-for fitsDict in fitsDicts:
+for fitsDict in flatFitsDicts:
     for key in fitsDict.keys():
         if key not in headers: headers.append(key)
 
@@ -64,16 +58,16 @@ csvFile = open(sys.argv[2]+'report.csv', 'w')
 pen = csv.writer(csvFile)
 pen.writerow(headers)
 
-for fitsDict in fitsDicts:
-    print('fitsDicts len: ' + str(len(fitsDicts)))
+for fitsDict in flatFitsDicts:
+    #print('fitsDicts len: ' + str(len(fitsDicts)))
     row = []
     currentColumn = 0
     for key in fitsDict.keys():
-        print('fitsDict len: ' + str(len(fitsDict.keys())))
+        #print('fitsDict len: ' + str(len(fitsDict.keys())))
         #print(json.dumps(fitsDict, indent=2))
         if key == headers[currentColumn]:
-            print('currentColumn: ' + str(currentColumn))
-            print('row length: ' + str(len(row)))
+            #print('currentColumn: ' + str(currentColumn))
+            #print('row length: ' + str(len(row)))
             row.append(fitsDict[key])
         else:
             row.append('')
@@ -81,5 +75,3 @@ for fitsDict in fitsDicts:
     pen.writerow(row)
 
 csvFile.close()
-
-print(json.dumps(open(sys.argv[2]+'reports.json').read(), indent=2))
