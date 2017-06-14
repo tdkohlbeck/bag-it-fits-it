b94d0fa7511895df154f3b1efeb2caa3c927d872
Travis Kohlbeck <tdkohlbeck+github@gmail.com>
2017-06-14 01:24:48 -0400

    change in logic, for loop changes, comments

--- bag-it-fits-it.py
+++ bag-it-fits-it.py
@@ -2,7 +2,10 @@
 
 import shutil, os, sys, subprocess
 import bagit
+import filecmp
 import json, csv, xmltodict, jsonstocsv
+
+# TODO create something like '"xml report" means "xmlFits"'?
 
 # directory locations
 dirToBag = sys.argv[1]
@@ -19,15 +22,15 @@ bag.save()
 shutil.copytree(masterBagDir, workingBagDir)
 
 # run FITS on working bag
-cmd = './fits/fits.sh -r -i ' + workingBagDir + ' -o ' + fitsXmlDir + ' -x'
+cmd = './fits/fits.sh -r -i ' + workingBagDir + '/data/ -o ' + fitsXmlDir + ' -x'
 subprocess.call(cmd, shell=True)
 
 def flattenDict(obj, delim):
     val = {}
-    for i in obj.keys():
+    for i in obj:
         if isinstance(obj[i], dict):
             get = flattenDict(obj[i], delim)
-            for j in get.keys():
+            for j in get:
                 val[ i + delim + j ] = get[j]
         else:
             val[i] = obj[i]
@@ -35,7 +38,8 @@ def flattenDict(obj, delim):
 
 # convert xml reports to dicts, compile in list
 flatFitsDicts = []
-for filename in os.listdir(fitsXmlDir):
+fitsReportFiles = sorted(os.listdir(fitsXmlDir))
+for filename in fitsReportFiles:
     fitsXmlReport = open(fitsXmlDir + filename)
     fitsDict = xmltodict.parse(fitsXmlReport.read())
     fitsXmlReport.close()
@@ -43,26 +47,32 @@ for filename in os.listdir(fitsXmlDir):
     flatFitsDicts.append(flatFitsDict)
 
 # place all dict keys in a list
-headers = []
+headers = ['filepath']
 for fitsDict in flatFitsDicts:
-    for key in fitsDict.keys():
-        if key not in headers: headers.append(key)
+    for key in sorted(fitsDict):
+        if key not in headers: headers.append(str(key))
 
+# TODO * make sure same number of headers every time
+#      * write list to file, diff files every run
+#      * overwrite file for every run?
 # write dict keys as csv column names
-csvFile = open(sys.argv[2]+'report.csv', 'w')
+csvFile = open(outputDir + 'report.csv', 'w')
 pen = csv.writer(csvFile)
 pen.writerow(headers)
 
+# TODO compare dict to row and header
 # write values to relevant columns
+currentRow = 0
 for fitsDict in flatFitsDicts:
-    row = []
-    currentColumn = 0
-    for key in fitsDict.keys():
-        if key == headers[currentColumn]:
-            row.append(fitsDict[key])
-        else:
-            row.append('')
-        currentColumn += 1
+    row = [ fitsReportFiles[currentRow] ]
+    print(currentRow)
+    for key in sorted(fitsDict):
+        for header in headers:
+            if key == header: row.append(fitsDict[key])
+            else: row.append('')
+            #if len(row) > 8: print(headers[len(row) % 8])
+
     pen.writerow(row)
+    currentRow += 1
 
 csvFile.close()
