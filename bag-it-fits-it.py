2b648ea1830a4314e42fc0e3d03e297ae407983c
Travis Kohlbeck <tdkohlbeck+github@gmail.com>
2017-06-18 20:42:02 -0400

    clean-up, windows compatiblizing, etc.

--- bag-it-fits-it.py
+++ bag-it-fits-it.py
@@ -11,23 +11,26 @@ import json, csv, xmltodict
 
 def error():
     lines = [
-        'please provide:',
-        '',
+        'please provide:\n',
         '1. a directory to bag-n-fits',
         '2. optionally a directory to place output',
-        'if no second directory is provided, original location will be bagged',
-        '',
+        'if no second directory is provided, original location will be bagged\n',
         'example: \> bag_it_fits_it.py /dir/to/bag /dir/to/output',
     ]
     for line in lines:
         os.system('echo ' + line)
 
+# do some pre-flight checks
 if len(sys.argv) <= 2:
     error()
     quit()
+fits_script = ''
+if platform.system() == 'Windows':
+    fits_script = 'fits\\fits.bat'
+else: fits_script = 'fits/fits.sh'
 
-dirToBag = sys.argv[1]
-outputDir = sys.argv[2]
+dirToBag = os.path.abspath(sys.argv[1])
+outputDir = os.path.abspath(sys.argv[2])
 masterBagDir = outputDir + '/master-bag'
 workingBagDir = outputDir + '/working-bag'
 fitsXmlDir = outputDir + '/fits-xml/'
@@ -47,10 +50,6 @@ if not os.path.exists('fits'):
     subprocess.call(cmd, shell=True)
 """
 
-fits_script = ''
-if platform.system() == 'Windows': fits_script = 'fits\\fits'
-else: fits_script = 'fits/fits.sh'
-
 # run FITS on working bag
 cmd = fits_script + ' -r -i ' + workingBagDir + '/data/ -o ' + fitsXmlDir + ' -x'
 subprocess.call(cmd, shell=True)
@@ -67,20 +66,9 @@ def flattenDict(obj, delim):
     return val
 
 # convert xml reports to dicts, compile in list
+# TODO: remove unneeded sorted?
 flatFitsDicts = []
 fitsReportFiles = sorted(os.listdir(fitsXmlDir))
-
-def checkReplace(name):
-    fh = open('/tmp/'+name+'.txt', 'r')
-    old = fh.read()
-    fh.close()
-    new = globals()[name]
-    print(name + ' changed: ' + str(str(old) != str(new)))
-    #print(old)
-    #print(new)
-    fh = open('/tmp/'+name+'.txt', 'w')
-    fh.write(str(globals()[name]))
-    fh.close()
 
 # convert xmls to dicts and squash 'em
 for filename in fitsReportFiles:
@@ -97,7 +85,7 @@ for fitsDict in flatFitsDicts:
         if key not in headers: headers.append(str(key))
 
 # write dict keys as csv column names
-csvFile = open(outputDir + 'report.csv', 'w')
+csvFile = open(outputDir + '/report.csv', 'w')
 pen = csv.writer(csvFile)
 pen.writerow(headers)
 
