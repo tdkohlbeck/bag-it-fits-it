579431979f5013a5b539408db617cf84bef5d8bf
Travis Kohlbeck <tdkohlbeck+github@gmail.com>
2017-07-10 01:32:00 -0400

    outlines fits and csv report objs

---
+++ class/fits.py
@@ -0,0 +1,23 @@
+class Fits:
+    fits_path = Path('.')
+
+    def __init__(
+        self,
+        input,
+        output
+        path=Path('.'),
+    ):
+        fits_path = path
+
+    # instance props
+    # - input dir
+    # - output dir
+    #
+    # class props
+    # - fits path
+    #
+    # methods
+    # - set_path(Path)
+    # - run()
+    # - !search_for_path(Path)
+    # -
---
+++ class/report.py
@@ -0,0 +1,38 @@
+class Report:
+
+    def __init__(
+        self,
+        xml
+    ):
+        # if xml is_dir
+        self.xml_dir = xml
+
+        # if file
+        # self.file = xml
+
+        self.rows = list
+
+    def xml_to_csv_row(self):
+        def xml_to_dict(xml):
+        def flatten_dict(dict):
+        def dict_to_csv(flat_dict):
+
+
+    def rows_to_report(self):
+
+"""
+xml -> dict
+dict -> flat dict
+flat dict -> csv row
+csv rows -> report
+
+instance props
+- xml file/dir
+- rows
+
+class props
+
+methods
+- xml -> row
+- rows -> csv
+"""
